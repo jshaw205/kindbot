2eac2ec6fc853e2c3553f610b124e819f59c2037
James Shaw <38634775+jshaw205@users.noreply.github.com>
2022-04-10 11:27:43 +0100

    Update racefuel.js

--- commands/racefuel.js
+++ commands/racefuel.js
@@ -13,6 +13,7 @@ module.exports = {
 				const lapTimeSeconds = (parseFloat(args[1].split(':').shift()) * 60) + parseFloat(args[1].split(':').pop());
 				const totalLaps = raceTimeSeconds / lapTimeSeconds;
 				const fuelEstimate = totalLaps * args[2] + 1;
+				// this line is a goof because paul never leaves 2nd gear - add 10 litres to whatever calculation we come up with
 				if (message.author.tag == 'ickel2000uk#8152') {
 					message.channel.send(`\`\`\`Estimated fuel: ${Math.ceil(fuelEstimate)}\nSafe fuel level (+5%): ${Math.ceil(fuelEstimate + (fuelEstimate * 0.05))}\nPaul level fuel: ${Math.ceil(fuelEstimate + 10)}\`\`\``);
 				}
